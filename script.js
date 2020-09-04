e202efc7c1b6d3b0c89e0e321fc4dbfc10ff9cfd
Amlak Befekadu <amlakbarkon@gmail.com>
2020-09-04 12:54:38 -0400

    i am going to add login button

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 console.log("javascript is working")
-let obj = document.body.getElementsByClassName("bi bi-cursor-fill")[0];
-obj.addEventListener("click", ()=>{
- obj.classList.add("flyme");
-})
+$('#myModal').on('shown.bs.modal', function () {
+    $('#myInput').trigger('focus')
+  })
